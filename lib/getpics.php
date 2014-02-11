1123c41280f706b6a69e33bff83b84720ee72d69
Orest Tarasiuk <orest.tarasiuk@tum.de>
2014-02-11 23:59:06 +0100

    create some structure

--- lib/getpics.php
+++ lib/getpics.php
@@ -4,7 +4,7 @@ Header("content-type: application/x-javascript");
 
 //This function gets the file names of all images in the current directory
 //and ouputs them as a JavaScript array
-function returnimages($dirname="./pictures") {
+function returnimages($dirname="../pictures") {
 	$pattern="(\.jpg$)|(\.png$)|(\.jpeg$)|(\.gif$)"; //valid image extensions
 	$files = array();
 	$curimage=0;
